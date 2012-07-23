b911f0b3f2575ee795f48b556c4678548dd67ac9
Apprentice <apprentice@devbootcamp.com>
2012-07-23 15:35:00 -0700

    Add pagination

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,15 +1,17 @@
 class LinksController < ApplicationController
-	# def index
-	# 	@links = Link.scoped.page(params[:page])
-	# 	@link = Link.new
-	# end
+	def index
+		@links = Link.scoped.page(params[:page])
+		# @links = Link.all
+		@link = Link.new
+	end
 
-	# def create
-	# 	@link = Link.new(params[:link])
-	# 	redirect_to :links
-	# end
+	def create
+		@link = Link.new(params[:link])
+		@link.save
+		redirect_to :links
+	end
 
-	# def new
-	# 	@link = Link.new
-	# end
+	def new
+		@link = Link.new
+	end
 end
