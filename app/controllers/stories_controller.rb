e0f8b08d018465123393a2f318afd0a4e521e87d
Apprentice <apprentice@devbootcamp.com>
2012-07-25 17:12:41 -0700

    make edit a button; make edit button visible only on new posts, and only to the owner of that post; append a ? to sentences ending without punctuation or uppercase

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -3,10 +3,12 @@ class StoriesController < ApplicationController
 	before_filter :correct_user, :only => [:edit, :update]
 	before_filter :find_id, :only => [:edit, :update, :show]
 
+
 	def index
 		@stories = Story.order('created_at DESC').page( params[:page] )
 		@story  = Story.new
 	end
+
 
 	def create
 		@story      = Story.new( params[:story] )
@@ -18,9 +20,11 @@ class StoriesController < ApplicationController
 		redirect_to :root
 	end
 
+
 	def new
 		@story = Story.new
 	end
+
 
 	def update
 		@story.update_attributes( params[:story] )
@@ -28,12 +32,15 @@ class StoriesController < ApplicationController
 		redirect_to :root
 	end
 
+
 	def show
 	end
+
 
 	def edit
 		time_limit_enforcement( @story )
 	end
+
 
 	private
 
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,8 +1,17 @@
 class Story < ActiveRecord::Base
 	belongs_to :user
+	before_save :sentence_finisher
 	validates :url, :uniqueness => true, :presence => true
 
 	def new?
 		( Time.zone.now - self.created_at ).to_i < 900
+	end
+
+	def sentence_finisher
+		self.title = self.title + "?" if self.title.match( /[^A-Z]$\b/ )
+	end
+
+	def belongs_to?(user)
+		self.user == user
 	end
 end
